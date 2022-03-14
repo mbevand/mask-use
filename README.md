0371b7907a92223cf1a4ec1b7f25ee568f3cc728
Marc Bevand <m@zorinaq.com>
2022-03-14 08:50:11 -0700

    initial import

---
+++ README.md
@@ -0,0 +1,41 @@
+# Comparing mask use to COVID-19 case numbers
+
+Ask 250,000 people the following question: "*How often do you wear a mask in public when you expect to be within six feet of another person?*" and their answers correlate with COVID-19 case numbers across 3,000+ United States counties:
+
+![Comparing mask use to COVID-19 case numbers](chart.png)
+
+## Interpretation
+
+Among the 3,114 US counties that could be matched between the mask usage survey data and the JHU CSSE dataset, masking scores range from about 35 to about 95.
+
+We find a moderate correlation between masking score and case numbers (R² = 0.240); low mask usage counties reported about 3 times more cases per capita than high mask usage counties.
+
+Note that this analysis does not control for other variables and is likely subject to confounding biases, eg. people who report more frequent mask usage probably follow stronger social distancing protocols.
+
+## Methodology
+
+### Survey
+
+The global data and survey firm Dynata, at the request of The New York Times, conducted a large number of interviews in the United States to obtain 250,000 survey responses about mask use, enough data to provide estimates more detailed than the state level: https://github.com/nytimes/covid-19-data/tree/master/mask-use
+
+The firm asked the question "*How often do you wear a mask in public when you expect to be within six feet of another person?*" Respondents could give one of five possible answers: never, rarely, sometimes, frequently, always. The survey was conducted between 2020-07-02 and 2020-07-14.
+
+Raw survey responses were then transformed into county-level estimates: the survey data was weighted by age and gender, and survey respondents’ locations were approximated from their ZIP codes. Then estimates of mask-wearing were made for each census tract by taking a weighted average of the 200 nearest responses, with closer responses getting more weight in the average. These tract-level estimates were then rolled up to the county level according to each tract’s total population.
+
+### Masking score
+
+From this survey data, we synthesize a *masking score* for each US county, based on the fraction of respondents who gave each answer:
+
+  masking score = 0 × NEVER + 25 × RARELY + 50 × SOMETIMES + 75 × FREQUENTLY + 100 × ALWAYS
+
+This produces a score between 0 and 100 indicating respectively that residents of the county never masks, or always wear masks.
+
+### COVID-19 case numbers
+
+For each county, we also obtained the cumulative number of COVID-19 cases recorded during the 6-month period following the survey (2020-07-01 to 2020-12-31). We use [JHU CSSE](https://github.com/CSSEGISandData/COVID-19) as the data source.
+
+The number of cases per capita is calculated based on population data also from JHU CSSE ([time_series_covid19_deaths_US.csv, column Population](https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv))
+
+### Linear regression
+
+We plot each US county on a scatterplot: their masking score are on the X axis, and cases per capita on the Y axis (log scale). Then Y values are log-transformed before calculating a linear regression.
---
+++ gen.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+
+import math, datetime
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from matplotlib import rcParams
+import matplotlib.pyplot as plt
+
+time_period = ('7/1/20', '12/31/20')
+
+def cd(d):
+    return datetime.datetime.strptime(d, '%m/%d/%y').strftime('%Y-%m-%d')
+
+def chart(counties):
+    rcParams['font.family'] = 'serif'
+    rcParams['font.serif'] = 'Latin Modern Roman'
+    rcParams['font.weight'] = 'bold'
+    rcParams['axes.labelweight'] = 'bold'
+    rcParams['axes.labelsize'] = 'x-large'
+    plt.style.use('dark_background')
+    (fig, ax) = plt.subplots(dpi=700, figsize=(7, 3))
+    ax.text(.45, 1, 'Mask Usage & COVID-19 Cases\nin United States Counties',
+            transform=ax.transAxes, ha='center', fontsize='x-large')
+    ax.text(1, .95, 'Marc Bevand\n@zorinaq',
+            transform=ax.transAxes, ha='right', fontsize='x-small')
+    ax.text(.5, -.21, 'Source: https://github.com/mbevand/mask-use',
+            transform=ax.transAxes, ha='center', va='top', fontsize='x-small')
+    ax.semilogy()
+    #ax.tick_params(axis='x', bottom=False, labelbottom=False)
+    for sp in ax.spines:
+        ax.spines[sp].set_visible(False)
+    ax.set_ylabel('Cases per million people')
+    ax.set_xlabel(f'← Less{" "*14}Mask usage{" "*14}More →')
+    x = [_['score'] for _ in counties.values()]
+    y = [_['rate'] for _ in counties.values()]
+    ax.scatter(x, y, color=(1,1,1,.7), marker='o', s=5)
+    # regression
+    reg = LinearRegression().fit(np.reshape(x, (-1, 1)), np.reshape(np.log(y), (-1, 1)))
+    preds = np.reshape(reg.predict(np.reshape(x, (len(x), -1))), (len(x),))
+    r2 = r2_score(np.log(y), preds)
+    print(f'R²={r2:.3f}')
+    x1, x2 = ax.get_xlim()
+    x = np.linspace(x1, x2)
+    y = np.exp(np.reshape(reg.predict(np.reshape(x, (len(x), -1))), (len(x),)))
+    ax.plot(x, y, c='red', lw=3)
+    ax.set_xlim(left=x1, right=x2)
+    ax.text(.05, .05, f'Linear reg: R²={r2:.3f}', transform=ax.transAxes,
+            ha='left', va='bottom', color='red')
+    fig.savefig('chart.png', bbox_inches='tight')
+    return
+
+def init():
+    # NYT mask use data
+    df = pd.read_csv('../nyt-covid-19-data/mask-use/mask-use-by-county.csv')
+    # Calculate masking score
+    df['score'] = \
+        0 * df['NEVER'] + \
+        25 * df['RARELY'] + \
+        50 * df['SOMETIMES'] + \
+        75 * df['FREQUENTLY'] + \
+        100 * df['ALWAYS']
+    counties = {}
+    for _, row in df.iterrows():
+        counties[int(row['COUNTYFP'])] = { 'score': float(row['score']) }
+    # population data
+    population = {}
+    df = pd.read_csv('../jhu-csse-COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv')
+    for _, row in df.iterrows():
+        fips = row['FIPS']
+        if math.isnan(fips):
+            continue
+        population[int(fips)] = int(row['Population'])
+    # JHU data
+    df = pd.read_csv('../jhu-csse-COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv')
+    for _, row in df.iterrows():
+        fips = row['FIPS']
+        if math.isnan(fips):
+            # skip rows without a FIPS code (affects just a handful of counties)
+            #print(f'skipping {row["Admin2"]}, {row["Province_State"]}')
+            continue
+        fips = int(fips)
+        pop = population[fips]
+        if not pop:
+            # skip rows lacking population data (typically "out of state",
+            # or not assigned to a county)
+            #print(f'skipping {row["Admin2"]}, {row["Province_State"]}')
+            continue
+        cases = int(row[time_period[1]]) - int(row[time_period[0]])
+        rate = 1e6 * cases / pop
+        if fips not in counties:
+            # skip counties for which we have no mask use data (mostly Puerto Rico)
+            #print(f'{fips}: unknown fips for {row["Admin2"]}, {row["Province_State"]}')
+            continue
+        counties[fips]['pop'] = pop
+        counties[fips]['cases'] = cases
+        counties[fips]['rate'] = rate
+    print(f'{len(counties)} counties loaded')
+    delete = set()
+    for (fips, h) in counties.items():
+        if h['cases'] < 1:
+            delete.add(fips)
+    for d in delete:
+        del(counties[d])
+    print(f'{len(counties)} counties kept')
+    return counties
+
+def main():
+    counties = init()
+    chart(counties)
+
+main()
